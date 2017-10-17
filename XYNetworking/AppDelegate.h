f9d65f1712bd18742d9ab295b8cb82cf15aca7e4
MyYY01 <371661665@qq.com>
2017-10-17 14:57:19 +0800

    init iOS project

---
+++ XYNetworking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XYNetworking
+//
+//  Created by xyhr on 2017/10/17.
+//  Copyright © 2017年 xyhr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XYNetworking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XYNetworking
+//
+//  Created by xyhr on 2017/10/17.
+//  Copyright © 2017年 xyhr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
